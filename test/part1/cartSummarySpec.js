34ca4bb30cb36d3f44988945e8b5babe78824210
Tom Bradley <bradley.tom.r@gmail.com>
2016-04-07 12:25:38 +0100

    Adds first test

---
+++ test/part1/cartSummarySpec.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+var CartSummary = require('./../../src/part1/cartSummary.js');
+
+describe('CartSummary', function() {
+  it('getSubtotal() should return 0 if no items are passed in', function() {
+    var cartSummary = new cartSummary([]);
+    expect(cartSummary.getSubtotal()).to.eq(0);
+  });
+});
