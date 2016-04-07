91f14fddcca8d250820744c3aeddfb686dd893fa
Tom Bradley <bradley.tom.r@gmail.com>
2016-04-07 13:00:28 +0100

    Implements correct subtotal function

--- src/part1/cartSummary.js
+++ src/part1/cartSummary.js
@@ -3,6 +3,11 @@ function CartSummary(items) {
 }
 
 CartSummary.prototype.getSubtotal = function() {
+  if(this._items.length) {
+    return this._items.reduce(function(subtotal, item) {
+      return subtotal += (item.quantity * item.price);
+    }, 0);
+  }
   return 0;
 };
 
